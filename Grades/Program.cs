c266a70fb3638facbf5aa695639a2a591ebd91b1
Sonia Kurtz <kurtzsonia@gmail.com>
2017-02-12 12:11:40 +0100

    Delegates and events

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -11,14 +11,33 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
+
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Scott's Grade Book";
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AvareGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+            WriteResults("Avarage", stats.AvareGrade);
+            WriteResults("Highest", (int)stats.HighestGrade);
+            WriteResults("Lowest", stats.LowestGrade);
+        }
+        static void WriteResults(string desciption, int result)
+        {
+            Console.WriteLine(desciption + ": " + result);
+        }
+
+        static void WriteResults(string desciption, float result)
+        {
+            Console.WriteLine($"{desciption}: {result:F2}");
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventsArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
         }
     }
 }
